c6acb0f13283ea312dcdc5202581f1298dd7204d
muzammiltech12 <152636646+muzammiltech12@users.noreply.github.com>
2024-02-27 20:27:36 +0000

    datatypes

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,5 +1,21 @@
 "use strict" //treat all js code as a newer version.
 //alert(3+3) we are using node js not browser.
-console.log(3
-    +3);
+//console.log(3+3);
     //code readability should be high
+    //console.log("MUZAMMIL")
+
+    let name = "MUZAMMIL"
+    let age = 18
+    let IsloggedIn = false
+
+    // number => 2 to the power of 53
+    // bigint   (mostly used in larger number)
+    // string => ""
+    // boolean => true/falsenode 01_basics/02_datatype
+    // null => stand alone value  (representation of empty value)
+    // undefined =>   (which is not defined currently)
+    // symbol => unique  (mostly use in react)
+    // object
+
+    console .log(typeof null)  //output object  
+    console .log(typeof undefined)  //output undefined
