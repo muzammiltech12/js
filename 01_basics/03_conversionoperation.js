024df119090c5748e86a97ee281ebf91f77bc744
muzammiltech12 <152636646+muzammiltech12@users.noreply.github.com>
2024-02-28 09:30:50 +0000

    conversion in datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,16 @@
+let score = "33abc"
+//const {score} = req.body
+//console.log(typeof score)
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+//"33" => 33
+//"33abc"=> NaN
+//true => 1; false => 0
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn)
+//1 => true; 0 => false
+//"" => false
+//"muzammil" => true
